5aed4eee1b3dfd6b697e2eb60a79357f1a977d2c
Justin Gardner <justin@rhynorater.com>
2024-12-23 06:02:33 -0500

    3.4.2.35

--- wpdatatables/templates/admin/dashboard/dashboard.inc.php
+++ wpdatatables/templates/admin/dashboard/dashboard.inc.php
@@ -337,12 +337,13 @@ $tableChartsCount = (int)WDTTools::getTablesCount('chart');
                                  </span>
                         </p>
                         <p class="wpdt-text wpdt-font m-b-18">
-                            New update with bug fixes:
+                            New update:
                         </p>
                         <div class="alert alert-info m-b-0" role="alert">
                             <i class="wpdt-icon-info-circle-full"></i>
                             <ul>
-                                <li><strong>Bugfix: </strong>Fixed issue with undefined method in WPDataChart class.</li>
+                                <li><strong>Improvement: </strong>Improved language translations.</li>
+                                <li><strong>BugFix: </strong>Minor CSS fixes.</li>
                                 <li>Other small bug fixes and stability improvements</li>
                             </ul>
                         </div>
